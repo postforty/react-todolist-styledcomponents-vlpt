48a5817aec1c99fe3444991bd3edf5ad8466ac8a
postforty <75767073+postforty@users.noreply.github.com>
2023-05-03 21:15:49 +0900

    Update TodoCreate.js

--- todolist-js/src/components/TodoCreate.js
+++ todolist-js/src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { MdAdd } from "react-icons/md";
 import styled, { css } from "styled-components";
+import { useTodoDispatch, useTodoNextId } from "./TodoProvider";
 
 const CircleButton = styled.button`
   background: #38d9a9;
@@ -76,17 +77,38 @@ const Input = styled.input`
 
 const TodoCreate = () => {
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
 
   const onToggle = () => setOpen(!open);
+  const onChange = (e) => setValue(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "CREATE",
+      todo: {
+        id: nextId.current,
+        text: value,
+        done: false,
+      },
+    });
+    setValue("");
+    setOpen(false);
+    nextId.current++;
+  };
 
   return (
     <>
       {open && (
         <InsertFormPositioner>
-          <InsertForm>
+          <InsertForm onSubmit={onSubmit}>
             <Input
               autoFocus
               placeholder="할 일을 입력 후, Enter 를 누르세요!"
+              onChange={onChange}
+              value={value}
             />
           </InsertForm>
         </InsertFormPositioner>
